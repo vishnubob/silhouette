1717d659e97300edec3164e9392152876c48a770
Giles Hall <giles@polymerase.org>
2014-08-08 15:15:25 -0400

    it moves!

--- silhouette.py
+++ silhouette.py
@@ -1,5 +1,17 @@
 import usb.core
 import usb.util
+import sys
+import re
+import time
+
+def transform_data(data):
+    data = data.split(' ')
+    res = []
+    for datum in data:
+        if datum[0] == '(' and datum[-1] == ')':
+            datum = datum[1:-1]
+            res.append(eval(datum))
+    return res
 
 # find our device
 dev = usb.core.find(idVendor=0x0b4d, idProduct=0x1123)
@@ -8,6 +20,9 @@ dev = usb.core.find(idVendor=0x0b4d, idProduct=0x1123)
 if dev is None:
     raise ValueError('Device not found')
 
+dev.reset()
+#print dev.get_active_configuration()
+
 # set the active configuration. With no arguments, the first
 # configuration will be the active one
 dev.set_configuration()
@@ -15,17 +30,69 @@ dev.set_configuration()
 # get an endpoint instance
 cfg = dev.get_active_configuration()
 
-for cfg in dev:
-    print str(cfg.bConfigurationValue)
-
 intf = cfg[(0,0)]
 
-ep = usb.util.find_descriptor(
+ep_out = usb.util.find_descriptor(
     intf,
-    # match the first OUT endpoint
     custom_match = \
     lambda e: \
         usb.util.endpoint_direction(e.bEndpointAddress) == \
         usb.util.ENDPOINT_OUT)
 
-assert ep is not None
+assert ep_out is not None
+
+ep_in = usb.util.find_descriptor(
+    intf,
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_IN)
+
+assert ep_in is not None
+
+ep_intr_in = usb.util.find_descriptor(intf, 
+        custom_match=lambda e: usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_IN and usb.util.endpoint_type(e.bEndpointAddress) == usb.util.ENDPOINT_TYPE_INTR)
+
+assert ep_intr_in is not None
+
+def run_script(fn):
+    f = open(fn)
+    state = None
+    for (line_num, line) in enumerate(f):
+        line = line.strip()
+        line = line.split(',')
+        pid = line[1]
+        if pid.startswith("DATA"):
+            data = line[5]
+            data = transform_data(data)
+            if state == "out":
+                ep_out.write(data)
+                print "sent", data, line_num
+            elif state == "in":
+                for x in range(10):
+                    try:
+                        res = ep_in.read(20)
+                    except:
+                        time.sleep(.1)
+                    print "got", res, line_num
+                    if list(res) != data:
+                        print "expected", data
+                    break
+        elif pid == "OUT":
+            state = "out"
+        elif pid == "IN":
+            state = "in"
+        elif pid == "SETUP":
+            state = "setup"
+        elif pid == "NAK":
+            state = "nak"
+        elif pid == "ACK":
+            state = "ack"
+        elif pid == "SOF":
+            state = "sof"
+        else:
+            state = None
+        prevline = line
+
+fn = sys.argv[1]
+run_script(fn)
